bca9be7aad741f55c6b2b124e9c3217745e7afa4
dhess <dan.hess@kippbayarea.org>
2019-05-15 09:46:40 -0700

    Add job model for associating user roles

--- accounts/models.py
+++ accounts/models.py
@@ -23,12 +23,22 @@ class Site(models.Model):
         ordering = ('name',)
 
 
+class Job(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.ForeignKey(Role, on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT, blank=True, null=True)
     employee_number =  models.IntegerField(blank=True)
     email = models.EmailField()
-    job_title = models.CharField(max_length=100, blank=True)
-    role = models.ForeignKey(Role, on_delete=models.PROTECT, blank=True, null=True)
+    job_title = models.ForeignKey(Job, on_delete=models.PROTECT, blank=True, null=True)
     site = models.ForeignKey(Site, on_delete=models.PROTECT, blank=True, null=True)
     avatar_url = models.URLField(max_length=2000, blank=True)
 
@@ -41,7 +51,7 @@ class Profile(models.Model):
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'job_title')
-    list_filter = ('site', 'role')
+    list_filter = ('site', 'job_title__role')
     search_fields = [
         'user__first_name',
         'user__last_name',
