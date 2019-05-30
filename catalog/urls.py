cea92e739ef3d66e9e30e61ee1df7d38efa908ed
dhess <dan.hess@kippbayarea.org>
2019-05-30 08:55:42 -0700

    DATA-1013 Add simple search controller and template skeleton

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("report/<int:report_id>", views.report, name="report"),
     path('feedback/<int:report_id>/', views.feedback_form, name='feedback_form'),
     path('favorite/<int:report_id>/', views.favorite_form, name='favorite_form'),
+    path("search", views.search, name="search"),
 ]
