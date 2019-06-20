2fb82cc9e0404231391a86c31112dbdb247f7a2a
dhess <dan.hess@kippbayarea.org>
2019-06-20 11:10:21 -0700

    DATA-751 Add handling for blanks

--- high_health/models.py
+++ high_health/models.py
@@ -17,8 +17,8 @@ class EssentialQuestion(models.Model):
 class Metric(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    essential_question = models.ForeignKey(EssentialQuestion, on_delete=models.PROTECT, null=True)
-    report = models.ForeignKey(Report, on_delete=models.SET_NULL, null=True)
+    essential_question = models.ForeignKey(EssentialQuestion, on_delete=models.PROTECT, null=True, blank=True)
+    report = models.ForeignKey(Report, on_delete=models.SET_NULL, null=True, blank=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -40,8 +40,8 @@ class SchoolLevel(models.Model):
 
 class School(models.Model):
     name = models.CharField(max_length=100)
-    school_level = models.ForeignKey(SchoolLevel, on_delete=models.PROTECT, null=True)
-    site = models.ForeignKey(Site, on_delete=models.SET_NULL, null=True)
+    school_level = models.ForeignKey(SchoolLevel, on_delete=models.PROTECT, null=True, blank=True)
+    site = models.ForeignKey(Site, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -76,7 +76,7 @@ class Measure(models.Model):
     metric = models.ForeignKey(Metric, on_delete=models.PROTECT)
     school = models.ForeignKey(School, on_delete=models.PROTECT)
     value = models.DecimalField(max_digits=5, decimal_places=2)
-    goal = models.ForeignKey(Goal, on_delete=models.SET_NULL, null=True)
+    goal = models.ForeignKey(Goal, on_delete=models.SET_NULL, null=True, blank=True)
     date = models.DateTimeField(auto_now=True)
 
     @property
