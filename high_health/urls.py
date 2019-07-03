e9e3301969ffe27c74eaa83631b9e1533c889789
dhess <dan.hess@kippbayarea.org>
2019-07-03 09:15:38 -0700

    Add school level filtering

--- high_health/urls.py
+++ high_health/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("high_health/", views.high_health, name="high_health"),
+    path("high_health/<str:school_level>", views.high_health, name="high_health"),
 ]
