be0c03ae1b1f470e7f361180472923710a3dbba3
dhess <dan.hess@kippbayarea.org>
2019-07-13 18:07:01 -0700

    DATA-1326 Update nginx confi settings

--- data_portal/settings.py
+++ data_portal/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_URL = "/staticfiles/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
+)
 SAML_FOLDER = os.path.join(BASE_DIR, "saml")
 
 SESSION_ENGINE = "django.contrib.sessions.backends.file"
