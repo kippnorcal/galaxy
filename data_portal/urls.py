ee024c194f7f76bc5297b0abd451b1d1aa224b6a
dhess <dan.hess@kippbayarea.org>
2019-08-03 12:02:38 -0700

    DATA-1012 Add high health endpoints

--- data_portal/urls.py
+++ data_portal/urls.py
@@ -3,10 +3,12 @@ from django.urls import include, path
 from rest_framework import routers
 from accounts.urls import router as account_router
 from analytics.urls import router as analytics_router
+from high_health.urls import router as high_health_router
 
 router = routers.DefaultRouter()
 router.registry.extend(account_router.registry)
 router.registry.extend(analytics_router.registry)
+router.registry.extend(high_health_router.registry)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- high_health/urls.py
+++ high_health/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register(r"essential_questions", views.EssentialQuestionViewSet)
+router.register(r"metrics", views.MetricViewSet)
+router.register(r"goals", views.GoalViewSet)
+router.register(r"measures", views.MeasureViewSet)
+
 
 urlpatterns = [
     path("high_health/", views.high_health, name="high_health"),
