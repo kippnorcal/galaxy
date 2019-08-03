65d4bff7c03a28501ed0d98944663c24e7543f2a
dhess <dan.hess@kippbayarea.org>
2019-08-02 17:31:22 -0700

    DATA-1012 Add account endpoints

---
+++ accounts/serializers.py
@@ -0,0 +1,49 @@
+from .models import Role, SchoolLevel, Site, Job, Profile
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "username", "is_active")
+        read_only_fields = ("email", "username")
+
+
+class RoleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Role
+        fields = ("id", "name")
+
+
+class JobSerializer(serializers.HyperlinkedModelSerializer):
+    role = serializers.PrimaryKeyRelatedField(queryset=Role.objects.all())
+
+    class Meta:
+        model = Role
+        fields = ("id", "name", "role")
+
+
+class SchoolLevelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = SchoolLevel
+        fields = ("id", "name", "display_name")
+
+
+class SiteSerializer(serializers.HyperlinkedModelSerializer):
+    school_level = serializers.PrimaryKeyRelatedField(
+        queryset=SchoolLevel.objects.all()
+    )
+
+    class Meta:
+        model = Site
+        fields = ("id", "name", "is_school", "school_level")
+
+
+class ProfileSerializer(serializers.HyperlinkedModelSerializer):
+    job_title = serializers.PrimaryKeyRelatedField(queryset=Job.objects.all())
+    site = serializers.PrimaryKeyRelatedField(queryset=Site.objects.all())
+
+    class Meta:
+        model = Profile
+        fields = ("id", "employee_number", "email", "job_title", "site")
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,21 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"roles", views.RoleViewSet)
+router.register(r"jobs", views.JobViewSet)
+router.register(r"school_levels", views.SchoolLevelViewSet)
+router.register(r"sites", views.SiteLevelViewSet)
+router.register(r"profiles", views.ProfileLevelViewSet)
+
 urlpatterns = [
+    path("api/", include(router.urls)),
     path("login/", views.saml_login, name="login"),
     path("logout/", views.saml_logout, name="logout"),
     path("acs/", views.acs, name="acs"),
     path("metadata/", views.metadata, name="metadata"),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
