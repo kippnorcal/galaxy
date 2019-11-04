1b59328af981dba341b54bed1bcda2ed0d20c52b
Susan Xiong <susanxiong@KBAS-C1MWJ1FKJ1WK.local>
2019-11-04 13:02:16 -0800

    PROJ-654 Convert models tests to pytest

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,73 +1,71 @@
-from django.test import TestCase
 from django.core.exceptions import ValidationError
+import pytest
+
 from accounts.models import Job, Role, Site, Profile
 from catalog.models import Report, Favorite
 
 
-class RoleModelTest(TestCase):
-    fixtures = ['testing']
-
-    def setUp(self):
+class TestRoleModel:
+    @pytest.fixture(autouse=True)
+    def setup(self, db, django_db_setup):
         self.role = Role.objects.get(pk=1)
 
     def test_string_representation(self):
-        self.assertEqual(str(self.role), self.role.name)
+        assert str(self.role) == self.role.name
 
     def test_name_max_length_100(self):
-        with self.assertRaises(ValidationError):
-            self.role.name = 'x' * 101
+        with pytest.raises(ValidationError):
+            self.role.name = "x" * 101
             self.role.full_clean()
 
-class JobModelTest(TestCase):
-    fixtures = ['testing']
 
-    def setUp(self):
+class TestJobModel:
+    @pytest.fixture(autouse=True)
+    def setup(self, db, django_db_setup):
         self.job = Job.objects.get(pk=1)
 
     def test_string_representation(self):
-        self.assertEqual(str(self.job), self.job.name)
-
+        assert str(self.job) == self.job.name
 
     def test_role_can_be_blank(self):
         self.job.role = None
         try:
             self.job.full_clean()
         except ValidationError:
-            self.fail('Blank role should not throw validation error')
+            pytest.fail("Blank role should not throw validation error")
 
     def test_name_max_length_100(self):
-        with self.assertRaises(ValidationError):
-            self.job.name = 'x' * 101
+        with pytest.raises(ValidationError):
+            self.job.name = "x" * 101
             self.job.full_clean()
 
-class SiteModelTest(TestCase):
-    fixtures = ['testing']
 
-    def setUp(self):
+class TestSiteModel:
+    @pytest.fixture(autouse=True)
+    def setup(self, db, django_db_setup):
         self.site = Site.objects.get(pk=1)
 
     def test_string_representation(self):
-        self.assertEqual(str(self.site), self.site.name)
+        str(self.site) == self.site.name
 
     def test_name_max_length_100(self):
-        with self.assertRaises(ValidationError):
-            self.site.name = 'x' * 101
+        with pytest.raises(ValidationError):
+            self.site.name = "x" * 101
             self.site.full_clean()
 
 
-class ProfileModelTest(TestCase):
-    fixtures = ['testing']
-
-    def setUp(self):
+class TestProfileModel:
+    @pytest.fixture(autouse=True)
+    def setup(self, db, django_db_setup):
         self.report = Report.objects.get(pk=1)
         self.profile = Profile.objects.get(pk=1)
 
     def test_string_representation(self):
-        self.assertEqual(str(self.profile), self.profile.email)
+        assert str(self.profile) == self.profile.email
 
     def test_employee_number_max_length_5(self):
-        with self.assertRaises(ValidationError):
-            self.profile.employee_number = 'x' * 6
+        with pytest.raises(ValidationError):
+            self.profile.employee_number = "x" * 6
             self.profile.full_clean()
 
     def test_employee_number_can_be_blank(self):
@@ -75,11 +73,11 @@ class ProfileModelTest(TestCase):
         try:
             self.profile.full_clean()
         except ValidationError:
-            self.fail('Blank employee_number should not throw validation error')
+            pytest.fail("Blank employee_number should not throw validation error")
 
     def test_avatar_url_max_length_5(self):
-        with self.assertRaises(ValidationError):
-            self.profile.avatar_url = 'x' * 2001
+        with pytest.raises(ValidationError):
+            self.profile.avatar_url = "x" * 2001
             self.profile.full_clean()
 
     def test_avatar_url_can_be_blank(self):
@@ -87,17 +85,12 @@ class ProfileModelTest(TestCase):
         try:
             self.profile.full_clean()
         except ValidationError:
-            self.fail('Blank avatar_url should not throw validation error')
+            pytest.fail("Blank avatar_url should not throw validation error")
 
     def test_profile_linked_to_favorites(self):
         before_count = self.profile.favorites.all().count()
-        favorite = Favorite(
-            profile=self.profile,
-            report=self.report
-        )
+        favorite = Favorite(profile=self.profile, report=self.report)
         favorite.save()
         after_count = self.profile.favorites.all().count()
-        self.assertNotEqual(before_count, after_count)
+        assert before_count != after_count
         del favorite
-
-
