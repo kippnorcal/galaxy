3dc70922a2921df76a6c34b8f5255c69c41bc107
Susan Xiong <susanxiong@KBAS-C1MWJ1FKJ1WK.local>
2019-11-05 11:57:36 -0800

    PROJ-654 Convert catalog tests to pytest

--- catalog/tests/models/test_category.py
+++ catalog/tests/models/test_category.py
@@ -1,50 +1,49 @@
-from django.test import TestCase
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
+import pytest
+
 from catalog.models import Category, SubCategory
 
 
-class CategoryModelTest(TestCase):
-    fixtures = ['testing']
-
-    def setUp(self):
+class TestCategoryModel:
+    @pytest.fixture(autouse=True)
+    def setUp(self, db, django_db_setup):
         self.category = Category.objects.get(pk=1)
 
     def test_string_representation(self):
-        self.assertEqual(str(self.category), self.category.name)
+        assert str(self.category) == self.category.name
 
     def test_name_max_length_100(self):
-        with self.assertRaises(ValidationError):
-            self.category.name = 'x' * 101
+        with pytest.raises(ValidationError):
+            self.category.name = "x" * 101
             self.category.full_clean()
 
     def test_is_active_default_true(self):
         c = Category(name="Test Category 2")
         c.save()
-        self.assertTrue(c.is_active)
+        assert c.is_active == True
 
 
-class SubCategoryModelTest(TestCase):
-    fixtures = ['testing']
-
-    def setUp(self):
+class TestSubCategoryModel:
+    @pytest.fixture(autouse=True)
+    def setUp(self, db, django_db_setup):
         self.sub_category = SubCategory.objects.get(pk=1)
 
     def test_string_representation(self):
-        self.assertEqual(str(self.sub_category), self.sub_category.name)
+        assert str(self.sub_category) == self.sub_category.name
 
     def test_name_max_length_100(self):
-        with self.assertRaises(ValidationError):
-            self.sub_category.name = 'x' * 101
+        with pytest.raises(ValidationError):
+            self.sub_category.name = "x" * 101
             self.sub_category.full_clean()
 
     def test_is_active_default_true(self):
         category = Category.objects.get(pk=1)
         c = SubCategory(name="Test SubCategory 2", category=category)
         c.save()
-        self.assertTrue(c.is_active)
+        assert c.is_active == True
 
     def test_category_fk_required(self):
-        with self.assertRaises(ValidationError):
+        with pytest.raises(ValidationError):
             self.sub_category.category = None
             self.sub_category.full_clean()
