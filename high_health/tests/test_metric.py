4c243e899c799e45d3c698cc5b595f3843696f4f
Susan Xiong <susanxiong@kbas-c1mwj1fkj1wk.attlocal.net>
2020-01-08 17:20:11 -0800

    PROJ-1193 Add high health metrics tests and fixtures

---
+++ high_health/tests/test_metric.py
@@ -0,0 +1,47 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import pytest
+
+from high_health.models import Metric, EssentialQuestion
+
+
+
+class TestMetricModel:
+    @pytest.fixture(autouse=True)
+    def setUp(self, db, django_db_setup):
+        self.metric = Metric.objects.get(pk=1)
+
+    def test_string_representation(self):
+        assert str(self.metric) == self.metric.name
+
+    def test_name_max_length_100(self):
+        with pytest.raises(ValidationError):
+            self.metric.name = "x" * 101
+            self.metric.full_clean()
+
+    def test_definition_can_be_blank(self):
+        self.metric.definition = None
+        try:
+            self.metric.full_clean()
+        except ValidationError:
+            pytest.fail("Blank definition should not throw validation error")
+    
+    def test_essential_question_can_be_null(self):
+        self.metric.essentialquestion = None
+        try:
+            self.metric.full_clean()
+        except ValidationError:
+            pytest.fail("Blank essential question should not throw validation error")
+
+    def test_report_can_be_null(self):
+        self.metric.report = None
+        try:
+            self.metric.full_clean()
+        except ValidationError:
+            pytest.fail("Blank report should not throw validation error")
+
+    def test_year(self):
+        today = datetime.date.today()
+        assert self.metric.year == today.year
