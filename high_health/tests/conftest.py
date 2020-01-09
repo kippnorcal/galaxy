3769a6df7b094573bf0996d4e5f3f81652501b18
Susan Xiong <susanxiong@kbas-c1mwj1fkj1wk.attlocal.net>
2020-01-08 16:18:11 -0800

    PROJ-1193 Add fixtures and tests for high health essential questions

---
+++ high_health/tests/conftest.py
@@ -0,0 +1,8 @@
+from django.core.management import call_command
+import pytest
+
+
+@pytest.fixture(scope="session")
+def django_db_setup(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        call_command("loaddata", "testing.json")
