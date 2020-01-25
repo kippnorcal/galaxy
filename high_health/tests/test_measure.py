3148be50424f954ff1fc651ab0b969b9f9d7e905
Susan Xiong <48801993+susanxiong@users.noreply.github.com>
2020-01-24 16:46:38 -0800

    Use Decimal to set explicitly decimal values

--- high_health/tests/test_measure.py
+++ high_health/tests/test_measure.py
@@ -8,6 +8,7 @@ import pytest
 from high_health.models import Goal, Measure, Metric
 from accounts.models import Site
 
+
 class TestMeasureModel:
     @pytest.fixture(autouse=True)
     def setUp(self, db, django_db_setup):
@@ -19,12 +20,12 @@ class TestMeasureModel:
 
     def test_value_max_digits_5(self):
         with pytest.raises(ValidationError):
-            self.measure.value = 1000.00
+            self.measure.value = Decimal("1000.00")
             self.measure.full_clean()
 
     def test_value_max_decimal_2(self):
         with pytest.raises(ValidationError):
-            self.measure.value = 0.111
+            self.measure.value = Decimal("0.111")
             self.measure.full_clean()
 
     def test_value_can_be_null(self):
@@ -33,7 +34,7 @@ class TestMeasureModel:
             self.measure.full_clean()
         except ValidationError:
             pytest.fail("Blank value should not throw validation error")
-    
+
     def test_date_default_value(self):
         today = datetime.date.today()
         assert self.measure.date == today
@@ -41,7 +42,7 @@ class TestMeasureModel:
     def test_is_current_default_value_is_true(self):
         measure = Measure()
         assert measure.is_current == True
-    
+
     def test_measure_year_returns_date_year(self):
         self.measure.date = datetime.date.today()
         assert self.measure.year == self.measure.date.year
@@ -53,11 +54,11 @@ class TestMeasureModel:
         for month in range(7, 13):
             self.measure.date = datetime.date(2019, month, 1)
             assert self.measure.school_year == "19-20"
-    
+
     def test_measure_month_returns_date_month(self):
         self.measure.date = datetime.date.today()
         assert self.measure.month == self.measure.date.month
-    
+
     def test_month_name_returns_expected_value(self):
         assert self.measure.month_name == calendar.month_abbr[self.measure.date.month]
 
@@ -65,8 +66,8 @@ class TestMeasureModel:
         test_goal = Goal.objects.get(pk=1)
         test_goal.school = Site.objects.get(pk=1)
         test_goal.metric = Metric.objects.get(pk=1)
-        test_goal.previous_outcome = Decimal('90.00')
-        test_goal.target = Decimal('100.00')
+        test_goal.previous_outcome = Decimal("90.00")
+        test_goal.target = Decimal("100.00")
         self.measure.school = Site.objects.get(pk=1)
         self.measure.metric = Metric.objects.get(pk=1)
         assert self.measure.goal == test_goal
