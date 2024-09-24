07367620fb7a97f24bf348a4883aa4026d1b3eb7
Mark Young <markyoung.ca@gmail.com>
2024-09-23 17:39:09 -0700

    Set goal_distance template tag to handle None

--- templatetags/high_health_helpers.py
+++ templatetags/high_health_helpers.py
@@ -72,24 +72,27 @@ def mom_color_eval(measure, previous):
 
 
 @register.filter
-def addstr(arg1, arg2):
+def addstr(arg1: str, arg2: str) -> str:
     """concatenate arg1 & arg2"""
     return str(arg1) + str(arg2)
 
 
 @register.filter
-def goal_distance(measure):
-    if measure.goal.goal_type == "ABOVE":
-        if measure.value < measure.goal.target:
-            return f"-{measure.goal.target - measure.value}% below goal"
-        else:
-            return f"+{measure.value - measure.goal.target}% above goal"
+def goal_distance(measure: Measure) -> str:
+    if measure.goal is not None:
+        if measure.goal.goal_type == "ABOVE":
+            if measure.value < measure.goal.target:
+                return f"-{measure.goal.target - measure.value}% below goal"
+            else:
+                return f"+{measure.value - measure.goal.target}% above goal"
 
-    else:
-        if measure.value > measure.goal.target:
-            return f"+{measure.value - measure.goal.target}% above goal"
         else:
-            return f"-{measure.goal.target - measure.value}% below goal"
+            if measure.value > measure.goal.target:
+                return f"+{measure.value - measure.goal.target}% above goal"
+            else:
+                return f"-{measure.goal.target - measure.value}% below goal"
+    else:
+        return "No goal set for this metric."
 
 
 register.filter("goal_format", goal_format)
